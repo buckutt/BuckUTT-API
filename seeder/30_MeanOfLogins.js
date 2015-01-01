8df4af52a5b90c1b84cda8b9390ffc4eafdd630f
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-01-01 21:28:07 +0100

    Mean Of Login seeder

---
+++ seeder/30_MeanOfLogins.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Promise   = require('bluebird');
+
+module.exports = function (sequelize) {
+  return new Promise(function (resolve, reject) {
+    console.log('Seeding MeanOfLogin');
+
+    var molNEtu = sequelize.MeanOfLogin.build({
+      name: 'NumeroEtu',
+    });
+
+    molNEtu.save().then(function () {
+      var molEtuName = sequelize.MeanOfLogin.build({
+        name: 'LoginEtu',
+      });
+      molEtuName.save().then(function () {
+        var molEtuMail = sequelize.MeanOfLogin.build({
+          name: 'EmailEtu'
+        });
+        molEtuMail.save().then(function () {
+          resolve();
+        })
+      });
+    }).catch(function (e) {
+      reject(e);
+    });
+  });
+};
