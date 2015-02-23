9db29ef1bfc4f5e47297be42e4729edf9dc5a061
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-02-23 16:18:21 +0100

    MeanOfLogin data can be string (login etu)

--- models/MeanOfLoginsUsers.js
+++ models/MeanOfLoginsUsers.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
         },
 
         data: {
-            type: DataTypes.INTEGER
+            type: DataTypes.STRING
         },
 
         isRemoved: {
