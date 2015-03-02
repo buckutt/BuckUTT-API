d90982761d2ebd22557b958bf905db74bec49747
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-03-02 12:36:21 +0100

    Multi-core back-end

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
  */
 
 var path           = require('path');
+var cluster        = require('cluster');
 var express        = require('express');
 var bodyParser     = require('body-parser');
 var morgan         = require('morgan');
@@ -15,38 +16,44 @@ var log            = libs.logManager(module);
 var middlewares    = require('./middlewares');
 var modelsAsync    = require('./models');
 
-
-var app = express()
-    .use(bodyParser.json())
-    .use(morgan('dev'))
-    .use(methodOverride())
-    .use(express.static(path.join(__dirname, './public')));
-
 var router = require('./routes/routes');
 
+var models;
 
-modelsAsync()
-    .then(function (models) {
-        var seeder = require('./seeder')(models, config.get('seed'));
+if (cluster.isMaster) {
+    modelsAsync()
+        .then(function (models) {
+            var seeder = require('./seeder')(models, config.get('seed'));
 
-        models.sequelize
-            .authenticate()
-            .then(seeder)
-            .then(function() {
-                app
-                    .use(middlewares.setModels(models))
-                    .use('/', router)
-                    .use(middlewares.pageNotFound)
-                    .use(middlewares.internalError)
-                    .listen(config.get('port'));
+            models.sequelize
+                .authenticate()
+                .then(seeder)
+                .then(function(models_) {
+                    models = models_;
+                    var numCPUs = require('os').cpus().length;
+                    for (var i = numCPUs - 1; i >= 0; --i) {
+                        cluster.fork();
+                    }
+                })
+                .catch(function(err) {
+                    throw new Error(err);
+                });
 
-                log.info('Server is listening on %d', config.get('port')); 
-            })
-            .catch(function(err) {
-                throw new Error(err);
-            });
+        });
+} else {
+    var app = express()
+        .use(bodyParser.json())
+        .use(morgan('dev'))
+        .use(methodOverride())
+        .use(express.static(path.join(__dirname, './public')))
 
-    });
+        .use(middlewares.setModels(models))
+        .use('/', router)
+        .use(middlewares.pageNotFound)
+        .use(middlewares.internalError)
+        .listen(config.get('port'));
 
+    log.info('Server is listening on %d from worker %d', config.get('port'), cluster.worker.id); 
+}
 
 module.exports = app;
