ac4ed6b90d9607f727230657330694e2e08234ec
Paul Chabanon <paul.chabanon@gmail.com>
2015-03-11 23:24:48 +0100

    add fundation behaviour on treasury purchase route

--- controllers/services/treasury.js
+++ controllers/services/treasury.js
@@ -29,28 +29,50 @@ module.exports = function() {
           return next(error);
       }
       var sequelize = req.models.sequelize;
-
-      var getPurchases = "\
-          SELECT\
-          fundations.name FundationName,\
-          points.name PointName,\
-          sum(purchases.price) Amount\
-          FROM\
-          purchases\
-          Inner Join points ON points.id = purchases.PointId\
-          Inner Join fundations ON fundations.id = purchases.FundationId\
-          where purchases.date between :DateStart and :DateEnd ";
-          
-      if (req.query.FundationId)
-        getPurchases += ' and purchases.FundationId = :FundationId ';
+      var getPurchases = '';
       
-      getPurchases += "\
-          GROUP BY\
-          purchases.FundationId,\
-          purchases.PointId\
-          ORDER BY\
-          purchases.FundationId,\
-          purchases.PointId";
+      if (req.query.FundationId){//Fundation specific
+          getPurchases += "\
+              SELECT\
+              count(purchases.`id`) PurchasesCount,\
+              sum(purchases.price) PurchasesSum,\
+              purchases.price ArticlePrice,\
+              articles.name ArticleName,\
+              points.name PointName\
+              FROM purchases\
+              Inner Join articles ON articles.id = purchases.ArticleId\
+              Inner Join points ON points.id = purchases.PointId\
+              WHERE purchases.date between :DateStart and :DateEnd AND ";
+          getPurchases += (req.query.split_promo ? 'articles.`type` =  \'product\'' : 'purchases.price <> 0');
+          getPurchases += " AND\
+              purchases.FundationId = :FundationId\
+              GROUP BY\
+              points.name,\
+              purchases.ArticleId,\
+              purchases.price\
+              ORDER BY\
+              points.name,\
+              articles.name,\
+              purchases.price";
+      }
+      else {//general treasury
+          getPurchases += "\
+              SELECT\
+              fundations.name FundationName,\
+              points.name PointName,\
+              sum(purchases.price) Amount\
+              FROM\
+              purchases\
+              Inner Join points ON points.id = purchases.PointId\
+              Inner Join fundations ON fundations.id = purchases.FundationId\
+              where purchases.date between :DateStart and :DateEnd\
+              GROUP BY\
+              purchases.FundationId,\
+              purchases.PointId\
+              ORDER BY\
+              purchases.FundationId,\
+              purchases.PointId";
+      }
       
       var opts = {
           replacements: {
@@ -141,6 +163,71 @@ module.exports = function() {
       
       sequelize
           .query(getClearance)
+          .then(function(results) {
+              res.json(utils.formatData(results[0]));
+          })
+          .catch(function(err) {
+              var error = new APIError(req, 
+                  'An uncatched error has been throwed', 
+                  'UNKNOWN_ERROR',
+                  500,
+                  { 
+                      error: err
+                  }
+              );
+
+              next(error);
+          });
+    });
+    
+    router.get('/test', function(req, res, next) {
+      /*if (!regDate.test(req.query.DateStart) || !regDate.test(req.query.DateEnd)) {
+          var error = new APIError(req, 
+              'Date format not valid', 
+              'BAD_FORMAT',
+              400
+          );
+
+          return next(error);
+      }//*/
+      var sequelize = req.models.sequelize;
+
+      var getReloads = "\
+          SELECT\
+          purchases.`date`,\
+          purchases.price,\
+          articles.id,\
+          articles.name,\
+          articles.`type`,\
+          points.name,\
+          purchases.BuyerId,\
+          users_b.firstname af,\
+          users_b.lastname al,\
+          users_s.firstname bf,\
+          users_s.lastname bl\
+          FROM\
+          purchases\
+          Inner Join articles ON articles.id = purchases.ArticleId\
+          Inner Join points ON points.id = purchases.PointId\
+          Inner Join users users_b ON users_b.id = purchases.BuyerId\
+          Inner Join users users_s ON users_s.id = purchases.SellerId\
+          WHERE\
+          purchases.`date` >=  '2013-12-05 05:53:31' AND\
+          purchases.`date` <=  '2013-12-05 19:30:00' AND\
+          purchases.FundationId = 3\
+          ORDER BY\
+          points.name,\
+          purchases.`date`";
+      
+      var opts = {
+          replacements: {
+              DateStart: req.query.DateStart,
+              DateEnd: req.query.DateEnd
+          }
+      };
+              
+      sequelize
+          .query(getReloads, opts)
           .then(function(results) {
               res.json(utils.formatData(results[0]));
           })
