963a0b1a0b7fcd008aacf6cfaa599a66d99e6522
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-09-24 13:47:18 +0200

    Maximum handled, errors handled

--- controllers/services/transfer.js
+++ controllers/services/transfer.js
@@ -22,7 +22,7 @@ module.exports = function (req, res, next) {
 
     if ((!amount && amount !== 0) || selfUser.credit < amount) {
         var error = new APIError(req,
-            'Buyer has not enough credit or credit is invalid',
+            'From user has not enough credit or credit is invalid',
             'BAD_VALUE',
             400,
             {
@@ -33,9 +33,21 @@ module.exports = function (req, res, next) {
         return reject(error);
     }
 
+    if (targetUser.credit + amount > 100 * 100) {
+        var error = new APIError(req,
+            'To user would have too much money (limit : 100€)',
+            'BAD_VALUE',
+            400,
+            {
+                amount: amount
+            }
+        );
+        return reject(error);
+    }
+
     User
         .find(req.body.userId)
-        .then(function (user) {
+        .then(function(user) {
             targetUser = user;
 
             targetUser.credit += amount;
@@ -55,4 +67,9 @@ module.exports = function (req, res, next) {
 
             return transfer.save();
         })
+
+        //Error handling
+        .catch(function(err) {
+            next(err);
+        });
 };
